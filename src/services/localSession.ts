a73c11280a0b51de16ff2c2c7af051e7599f3bbf
deluxscript <deluxscript@gmail.com>
2025-02-02 08:56:11 +0100

    refactor unused files

--- src/services/localSession.ts
+++ src/services/localSession.ts
@@ -1,12 +1,6 @@
 import { LocalSessionType } from '@/utils/LocalSessionType.ts'
-import { getLocalSession, setLocalSession } from '@/utils/localStorage.ts'
+import { setLocalSession } from '@/utils/localStorage.ts'
 
 export const toLocalSession = (data: Partial<LocalSessionType>) => {
   setLocalSession(data)
-}
-
-export const fromLocalSession = (): LocalSessionType | null => {
-  const session = getLocalSession()
-  if (!session) return null
-  return session
 }
